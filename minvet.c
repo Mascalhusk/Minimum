c68c018a7f68add54298c32924871db200b87053
Henrique Mascalhusk <hmascalhusk@id.uff.br>
2017-10-04 00:26:25 -0300

    ReadAndWrite

--- minvet.c
+++ minvet.c
@@ -1,22 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define tam 1369
 
-void ler(double *E, FILE*leitura)
+void ReadAndWrite(double *E, double *P1, double *P2, FILE*leitura)
 {	int i;
-	double a;
+	double a, b, c;
 	
-	while(fscanf(leitura, "%lf", &a)!=EOF)
-	{	E[i] = a; i++;		}
-	
+	while(fscanf(leitura, "%lf %lf %lf", &a, &b, &c)!=EOF)
+	{	P1[i] = a; P2[i] = b; E[i] = c;	
+		printf("%lf\t %lf\t %lf \n", P1[i], P2[i] ,E[i]) ; i++;	
+	}
+	printf("\n");
 }
 
 int main(int argc, char *argv[])
-{	double *E, a;
+{	double *E, *P1, *P2;
 	FILE *leitura;
-
-	E = malloc(sizeof(double));
 	leitura = fopen(argv[1], "r");
 
-	ler(E, leitura);
+	E = malloc((tam)*sizeof(double));
+	P1 = malloc((tam)*sizeof(double));
+	P2 = malloc((tam)*sizeof(double));
+	
+	ReadAndWrite(E, P1, P2, leitura);
 
 }
